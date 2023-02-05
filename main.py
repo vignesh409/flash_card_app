42c1e6a170fc3c046957a133219a1aadd70266d6
vignesh <vigneshvijaya0409@gmail.com>
2023-02-05 10:30:47 +0530

    Flash card app

---
+++ main.py
@@ -0,0 +1,62 @@
+from tkinter import *
+import pandas as pd
+import random
+
+
+
+
+BACKGROUND_COLOR = "#B1DDC6"
+data=pd.read_csv("data/french_words.csv")
+to_learn=data.to_dict(orient="records")
+current_card={}
+
+
+
+def next_card():
+    global  current_card,flip_timer
+    window.after_cancel(flip_timer)
+    current_card=random.choice(to_learn)
+    canvas.itemconfig(card_title,text='French',fill="black")
+    canvas.itemconfig(card_word,text=current_card['French'],fill="black")
+    canvas.itemconfig(card_background,image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title,text="English",fill="white")
+    canvas.itemconfig(card_word,text=current_card["English"],fill="white")
+    canvas.itemconfig(card_background,image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    next_card()
+
+
+window=Tk()
+window.title("Flash")
+window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
+
+flip_timer=window.after(3000,func=flip_card)
+
+canvas=Canvas(width=800,height=526)
+card_front_img=PhotoImage(file="images/card_front.png")
+card_back_img=PhotoImage(file="images/card_back.png")
+card_background=canvas.create_image(400,263,image=card_front_img)
+card_title=canvas.create_text(400,150,text='',font=("Courier New",40,"italic"))
+card_word=canvas.create_text(400,263,text='',font=("Courier New",60,"bold"))
+canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
+canvas.grid(row=0,column=0,columnspan=2)
+
+cross=PhotoImage(file="images/wrong.png")
+unknown_button=Button(image=cross,highlightthickness=0,command=next_card)
+unknown_button.grid(row=1,column=0)
+
+correct=PhotoImage(file="images/right.png")
+correct_button=Button(image=correct,highlightthickness=0,command=is_known)
+correct_button.grid(row=1,column=1)
+
+
+next_card()
+
+window.mainloop()
